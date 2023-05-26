eb5db57ad6764b6ba4106febf30efb0dd4078b89
muranakar <p1211065@gmail.com>
2023-05-26 14:41:42 +0900

    Initial Commit

---
+++ GitHubSearchMVP/GitHubSearchMVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSearchMVPApp.swift
+//  GitHubSearchMVP
+//
+//  Created by 村中令 on 2023/05/26.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSearchMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
